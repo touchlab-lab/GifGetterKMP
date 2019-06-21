31b2909353af4c269381d6fffaa5caf57da94f59
Justin Mancinelli <justin.mancinelli@gmail.com>
2019-06-21 15:27:10 -0400

    modify page for image carousel

--- jsApp/src/App.js
+++ jsApp/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Carousel interval="99999">
+          <Carousel.Item>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Carousel.Caption>
+              <h3>1</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Carousel.Caption>
+              <h3>2</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </header>
     </div>
   );
